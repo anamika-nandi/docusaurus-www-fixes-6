c27ef15e22fdc1dfe65d8933f228f4a0e9c6d035
anamika-nandi <nandianamika143@gmail.com>
2025-03-12 20:20:53 +0530

    Refactor SectionImg component for improved layout and styling

--- src/components/app/section-img.tsx
+++ src/components/app/section-img.tsx
@@ -6,25 +6,40 @@ import BrowserOnly from "@docusaurus/BrowserOnly";
 import lot from "./hero-animation.json";
 
 const LottieAnimation = () => {
-	return (
-		<BrowserOnly>
-			{() => {
-				// Dynamic import of Lottie
-				const Lottie = require("lottie-react").default;
-				return <Lottie animationData={lot} className='absolute top-0 left-0 w-full h-full md:h-auto md:w-auto' />;
-			}}
-		</BrowserOnly>
-	);
+  return (
+    <BrowserOnly>
+      {() => {
+        // Dynamic import of Lottie
+        const Lottie = require("lottie-react").default;
+        return (
+          <Lottie
+            animationData={lot}
+            className="absolute top-0 left-0 w-full h-full md:h-auto md:w-auto"
+          />
+        );
+      }}
+    </BrowserOnly>
+  );
 };
 
 export function SectionImg() {
-	return (
-		<div className='h-[370px] md:h-[60vw] 2xl:h-screen relative w-full'>
-			<LottieAnimation />
-			<img src={useBaseUrl("img/up-shadow.png")} alt='#' className='absolute h-[300px] md:h-auto -top-2 z-10 right-0 left-0 w-full ' />
-			<img src={useBaseUrl("img/down.png")} alt='#' className='absolute md:-bottom-4  h-[300px] md:h-auto bottom-0 right-0 left-0 w-full opacity-70 z-10' />
-			{/* The section image now fills the container (h-full) and is pulled upward */}
-			{/* <img src={useBaseUrl("img/section-img.png")} alt='#' className='w-full' /> */}
-		</div>
-	);
+  return (
+    <div className="bg-brand-black-100">
+      <div className="h-[370px] md:h-[60vw] 2xl:h-screen relative w-full container-custom bg-brand-black-100 ">
+        <LottieAnimation />
+        <img
+          src={useBaseUrl("img/up-shadow.png")}
+          alt="#"
+          className="absolute h-[300px] md:h-auto -top-2 z-10 right-0 left-0 w-full "
+        />
+        <img
+          src={useBaseUrl("img/down.png")}
+          alt="#"
+          className="absolute md:-bottom-4  h-[300px] md:h-auto bottom-0 right-0 left-0 w-full opacity-70 z-10"
+        />
+        {/* The section image now fills the container (h-full) and is pulled upward */}
+        {/* <img src={useBaseUrl("img/section-img.png")} alt='#' className='w-full' /> */}
+      </div>
+    </div>
+  );
 }
